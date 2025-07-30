d5e8dedbb1b59ae3ccad005f6339e04a36be5065
omarxar71 <omarabdrabi123@gmail.com>
2025-07-30 06:12:00 +0300

    commit

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+ * Load `$localize` - used for internationalization (i18n).
+ */
+import '@angular/localize/init';
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
